96b29e3e6fe4c0004f7f5c4a9016b8060d8dc92d
dheeraj0007 <dheeraj1863.be20@chitkara.edu.in>
2023-07-07 15:53:53 +0530

    Improving the download feature

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ showNotes();
 
 // if user adds a note , add it to the localStorage
 let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener('click',(e)=>{
+addBtn.addEventListener('click', (e) => {
     let addTxt = document.getElementById("addTxt");
     let notes = localStorage.getItem("notes");
-    if(notes==null){
+    if (notes == null) {
         notesObj = [];
     }
-    else{
+    else {
         notesObj = JSON.parse(notes);
     }
     console.log(notesObj);
-    if(addTxt.value!=""){
+    if (addTxt.value != "") {
         notesObj.push(addTxt.value);
     }
-    localStorage.setItem("notes",JSON.stringify(notesObj));
+    localStorage.setItem("notes", JSON.stringify(notesObj));
     addTxt.value = "";
     console.log(notesObj);
     showNotes();
@@ -24,17 +24,17 @@ addBtn.addEventListener('click',(e)=>{
 
 
 // Function to show elements from localStorage
-function showNotes(){
+function showNotes() {
     let notes = localStorage.getItem("notes");
-    if(notes==null){
+    if (notes == null) {
         notesObj = [];
     }
-    else{
+    else {
         notesObj = JSON.parse(notes);
     }
     let html = "";
-    notesObj.forEach((element,index) => {
-        html+=`
+    notesObj.forEach((element, index) => {
+        html += `
         <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
             <div class="card-body">
             <h5 class="card-title">Note ${index + 1}</h5>
@@ -45,59 +45,59 @@ function showNotes(){
             `;
     });
     let notesElm = document.getElementById("notes");
-    if(notesObj.length!=0){
+    if (notesObj.length != 0) {
         notesElm.innerHTML = html;
     }
-    else{
+    else {
         notesElm.innerHTML = `Nothing to show ! Use "Add a Note" section above to add notes`;
     }
 }
 
 // Function to delete a note
-function deleteNote(index){
+function deleteNote(index) {
     // console.log('I am deleting',index);
     let addTxt = document.getElementById("addTxt");
     let notes = localStorage.getItem("notes");
-    if(notes==null){
+    if (notes == null) {
         notesObj = [];
     }
-    else{
+    else {
         notesObj = JSON.parse(notes);
     }
-    notesObj.splice(index,1);
-    localStorage.setItem("notes",JSON.stringify(notesObj));
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
     showNotes();
 };
 
 
 let removeAll = document.getElementById("removeAll");
-removeAll.addEventListener('click',deleteAll);
+removeAll.addEventListener('click', deleteAll);
 
-function deleteAll(){
+function deleteAll() {
     let addTxt = document.getElementById("addTxt");
     let notes = localStorage.getItem("notes");
-    if(notes==null){
+    if (notes == null) {
         notesObj = [];
     }
-    else{
+    else {
         notesObj = JSON.parse(notes);
     }
-    notesObj.splice(0,notesObj.length);
-    localStorage.setItem("notes",JSON.stringify(notesObj));
+    notesObj.splice(0, notesObj.length);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
     showNotes();
 }
 
 let search = document.getElementById("searchTxt");
-search.addEventListener("input",()=>{
+search.addEventListener("input", () => {
     let inputVal = search.value.toLowerCase();
     // console.log("Input event fired!",inputVal);
     let notesCards = document.getElementsByClassName("noteCard");
-    Array.from(notesCards).forEach((element)=>{
+    Array.from(notesCards).forEach((element) => {
         let cardTxt = element.getElementsByTagName("p")[0].innerText;
-        if(cardTxt.includes(inputVal)){
+        if (cardTxt.includes(inputVal)) {
             element.style.display = "block";
         }
-        else{
+        else {
             element.style.display = "none";
         }
         // console.log(cardTxt);
@@ -106,14 +106,17 @@ search.addEventListener("input",()=>{
 
 let content = document.body.innerHTML;
 let downloadNotes = document.getElementById("downloadNotes");
-downloadNotes.addEventListener('click',()=>{
-    // console.log(content);
-    // let notesCards = document.getElementsByClassName("noteCard");
-    // document.body.innerHTML = "";
-    // for(let i = 0;i<notesCards.length;i++){
-    //     document.body.innerHTML+=notesCards[i].getElementsByClassName("card-text")[0];
-    // }
-    // document.body.innerHTML = content;
-    // console.log(content);
+downloadNotes.addEventListener('click', () => {
+    let notes = localStorage.getItem("notes");
+    let notesObj = JSON.parse(notes);
+    console.log(notesObj);
+    let everything = document.getElementById("everything").innerHTML;
+    let output = "<div style='font-size:20px'>";
+    for (let i = 0; i < notesObj.length; i++) {
+        output += `<p>${notesObj[i]}</p>`
+    }
+    output += "</div>";
+    document.getElementById('everything').innerHTML = output;
     window.print();
+    document.getElementById('everything').innerHTML = everything;
 });
